aaa3244cd3ce0c822466343ac617a8c39a3a8558
David-S-M-F <david.munoz-f@mail.escuelaing.edu.co>
2023-02-13 14:46:53 -0500

    Tarea 1

---
+++ CMakeFiles/Promedio_10_num.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Promedio_10_num.dir/Programa_10_num.cpp.o"
+  "CMakeFiles/Promedio_10_num.dir/Programa_10_num.cpp.o.d"
+  "Promedio_10_num"
+  "Promedio_10_num.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Promedio_10_num.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
